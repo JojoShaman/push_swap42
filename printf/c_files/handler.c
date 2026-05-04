8cf637aa84bcab3021b17b8ee8721d2a8f854c91
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-05-05 00:58:00 +0200

    fixed the memory leaks issue

--- printf/c_files/handler.c
+++ printf/c_files/handler.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                       :::      ::::::::    */
-/*   type_is.c                                         :+:      :+:    :+:    */
+/*   handler.c                                         :+:      :+:    :+:    */
 /*                                                   +:+ +:+         +:+      */
 /*   By: srosu <srosu@student.42belgium.be>        #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/04/08 17:06:23 by srosu            #+#    #+#              */
-/*   Updated: 2026/04/24 16:18:28 by srosu           ###   ########.fr        */
+/*   Updated: 2026/05/05 00:49:46 by srosu           ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ static void	check_ptr_p(unsigned long p, int *count)
 	}
 }
 
-void	type_is(char c, va_list *ap, int *count)
+void	type_is(char c, va_list * ap, int *count)
 {
 	unsigned long	arg;
 
