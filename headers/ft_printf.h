baac60386ce7effde6128cd63cd82e133a6d2b80
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-05-07 00:46:12 +0200

    feat : add fd feature

--- headers/ft_printf.h
+++ headers/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: srosu <srosu@student.42belgium.be>        #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/04/07 15:20:36 by srosu            #+#    #+#              */
-/*   Updated: 2026/04/24 15:53:01 by srosu           ###   ########.fr        */
+/*   Updated: 2026/05/07 00:08:05 by srosu           ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int		ft_printf(const char *format, ...);
-void	type_is(char c, va_list *ap, int *count);
+int		ft_printf(int fd, const char *format, ...);
+void	type_is(int fd, char c, va_list * ap, int *count);
 void	ft_putchar_fd(int c, int fd, int *count);
-void	ft_putnbr_base(unsigned long nb, char *base, int *count);
+void	ft_putnbr_base(int fd, unsigned long nb, char *base, int *count);
 void	ft_putstr_fd(char *s, int fd, int *count);
 void	ft_putnbr_fd(long long n, int fd, int *count);
 
