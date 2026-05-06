ff3efb7a8b2c340c1fd6a3eaa94e614bfe591d5f
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-05-07 00:24:03 +0200

    feat(push_swap.c): adapt_strategy function implementation & check_flag

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: srosu <srosu@student.42belgium.be>        #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/04/21 14:10:15 by srosu            #+#    #+#              */
-/*   Updated: 2026/05/06 16:20:37 by srosu           ###   ########.fr        */
+/*   Updated: 2026/05/07 00:18:10 by srosu           ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,20 @@ int	handle_argv2(char **argv, int i, t_data **stack)
 	return (-1);
 }
 
+void	adapt_strategy(t_data *s, float disorder, int *count)
+{
+	if (disorder < 0.2)
+	{
+		simple_sort(&s->stack_a, &s->stack_b, count);
+		s->big_o.strategy = "O\033[3m(n^2)\033[0m";
+	}
+	else
+	{
+		radix_sort(&s->stack_a, &s->stack_b, count);
+		s->big_o.strategy = "O\033[3m(n log n)\033[0m";
+	}
+}
+
 void	handle_stack(t_data *s)
 {
 	float	disorder;
@@ -36,14 +50,28 @@ void	handle_stack(t_data *s)
 	count = 0;
 	sort_array(t, (s->stack_a.tail->current_position + 1));
 	replace_value(t, &s->stack_a, (s->stack_a.tail->current_position + 1));
-	//radix_sort(&s->stack_a, &s->stack_b, &count);
-	simple_sort(&s->stack_a, &s->stack_b, &count);
-	ft_printf("\n");
-	print_bench(*s, count, percent / 100, percent % 100);
+	adapt_strategy(s, disorder, &count);
+	if (s->flags.bench_mode)
+		print_bench(*s, count, percent / 100, percent % 100);
 	free(t);
 	ft_lstclear(&s->stack_a);
 	ft_lstclear(&s->stack_b);
 	free(s);
+}
+
+int	check_flag(char *str, t_bool *flags)
+{
+	if (ft_strcmp(str, "--bench") == 0)
+	{
+		flags->bench_mode = 1;
+		return (1);
+	}
+	if (ft_strcmp(str, "--adaptive") == 0)
+	{
+		flags->adaptive = 1;
+		return (1);
+	}
+	return (0);
 }
 
 int	handle_main(int argc, char **argv, t_data **stack)
@@ -52,30 +80,36 @@ int	handle_main(int argc, char **argv, t_data **stack)
 	char	*tmp;
 	int		i;
 	int		j;
+	t_bool	flags;
 
 	str = NULL;
 	tmp = NULL;
 	j = 0;
 	i = 1;
+	memset(&flags, 0, sizeof(t_bool));
+	//remplacer par ft_memset
 	if (argc > 2)
 	{
 		while (i < argc)
 		{
-			if (!check_argv(argv[i]))
+			if (!check_flag(argv[i], &flags))
 			{
-				if (str)
-					free(str);
-				return (error(2));
-			}
-			tmp = str;
-			str = ft_strjoin(tmp, argv[i]);
-			if (tmp)
-				free(tmp);
-			if (i != argc - 1)
-			{
+				if (!check_argv(argv[i]))
+				{
+					if (str)
+						free(str);
+					return (error(2));
+				}
 				tmp = str;
-				str = ft_strjoin(tmp, " ");
-				free(tmp);
+				str = ft_strjoin(tmp, argv[i]);
+				if (tmp)
+					free(tmp);
+				if (i != argc - 1)
+				{
+					tmp = str;
+					str = ft_strjoin(tmp, " ");
+					free(tmp);
+				}
 			}
 			i++;
 		}
@@ -86,6 +120,7 @@ int	handle_main(int argc, char **argv, t_data **stack)
 				free(str);
 			return (1);
 		}
+		(*stack)->flags = flags;
 	}
 	if (str)
 		free(str);
