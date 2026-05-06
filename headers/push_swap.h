dc55bf2f8945e7fc64dfa105c21a912337e337da
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-05-07 00:47:51 +0200

    feat(struct): added and implemented t_strat & t_bool struct

--- headers/push_swap.h
+++ headers/push_swap.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mbuchet <mbuchet@student.42belgium.be>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/21 14:10:17 by srosu             #+#    #+#             */
-/*   Updated: 2026/05/06 17:06:33 by mbuchet          ###   ########.fr       */
+/*                                                       :::      ::::::::    */
+/*   push_swap.h                                       :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: srosu <srosu@student.42belgium.be>        #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/04/21 14:10:17 by srosu            #+#    #+#              */
+/*   Updated: 2026/05/06 23:39:12 by srosu           ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,20 @@ typedef struct s_list
 {
 	int				value;
 	int				current_position;
-	struct s_list	*target_node;
-	int				cost;
-	int				above_median;
 	struct s_list	*prev;
 	struct s_list	*next;
 }	t_list;
+
+typedef struct s_bool
+{
+	int	bench_mode;
+	int	adaptive;
+}	t_bool;
+
+typedef struct s_strat
+{
+	char	*strategy;
+}	t_strat;
 
 typedef struct s_opp
 {
@@ -51,6 +59,8 @@ typedef struct s_stack
 	t_list	*head;
 	t_list	*tail;
 	t_opp	*opp;
+	t_bool	*bool;
+	t_strat	*s_ptr;
 }	t_stack;
 
 typedef struct s_data
@@ -58,6 +68,8 @@ typedef struct s_data
 	t_stack	stack_a;
 	t_stack	stack_b;
 	t_opp	opp;
+	t_bool	flags;
+	t_strat	big_o;
 }	t_data;
 
 t_data	*create_stack_a(const char *str);
@@ -90,5 +102,6 @@ void	sort_array(int *tab, int size);
 void	simple_sort(t_stack *a, t_stack *b, int *count);
 void	print_bench(t_data stack, int count, int whole, int frac);
 int		word_len(char const *s, int start, char c);
+int		ft_strcmp(const char *s1, const char *s2);
 
 #endif
