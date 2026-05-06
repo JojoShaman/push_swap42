cf429b4684a41a0c44d57b53eb04b1212c0f16a0
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-05-06 04:42:08 +0200

    fix(swap): fix norminette

--- swap.c
+++ swap.c
@@ -1,42 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                       :::      ::::::::    */
-/*   swap.c                                            :+:      :+:    :+:    */
-/*                                                   +:+ +:+         +:+      */
-/*   By: srosu <srosu@student.42belgium.be>        #+#  +:+       +#+         */
-/*                                               +#+#+#+#+#+   +#+            */
-/*   Created: 2026/04/27 15:26:42 by srosu            #+#    #+#              */
-/*   Updated: 2026/05/06 01:41:56 by srosu           ###   ########.fr        */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/27 15:26:42 by srosu             #+#    #+#             */
+/*   Updated: 2026/05/06 04:41:52 by mbuchet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	swap(t_stack *stack, char c, int *count)
+static void	swap_stack(t_stack *stack)
 {
 	t_list	*first_element;
 	t_list	*second_element;
 	t_list	*third_element;
 
+	first_element = stack->head;
+	second_element = stack->head->next;
 	third_element = NULL;
-	if (stack->head && stack->head->next)
+	if (stack->head->next->next)
+		third_element = stack->head->next->next;
+	second_element->next = first_element;
+	second_element->prev = NULL;
+	first_element->prev = second_element;
+	stack->head = second_element;
+	if (third_element)
 	{
-		first_element = stack->head;
-		second_element = stack->head->next;
-		if (stack->head->next->next)
-			third_element = stack->head->next->next;
-		second_element->next = first_element;
-		second_element->prev = NULL;
-		first_element->prev = second_element;
-		stack->head = second_element;
-		if (third_element)
-		{
-			first_element->next = third_element;
-			third_element->prev = first_element;
-		}
-		else
-			first_element->next = NULL;
+		first_element->next = third_element;
+		third_element->prev = first_element;
 	}
+	else
+		first_element->next = NULL;
+}
+
+void	swap(t_stack *stack, char c, int *count)
+{
+	if (stack->head && stack->head->next)
+		swap_stack(stack);
 	if (c == 'a')
 		stack->opp->sa++;
 	else
