13513aaee1a10f3416a7fde489ebe04e6540bb6b
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-05-07 20:42:32 +0200

    fix(disorder): init mistakes in count_disorder & refactor compute_disorder

--- compute_disorder.c
+++ compute_disorder.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   compute_disorder.c                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mbuchet <mbuchet@student.42belgium.be>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/01 18:56:26 by srosu             #+#    #+#             */
-/*   Updated: 2026/05/07 17:58:54 by mbuchet          ###   ########.fr       */
+/*                                                       :::      ::::::::    */
+/*   compute_disorder.c                                :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: srosu <srosu@student.42belgium.be>        #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/05/01 18:56:26 by srosu            #+#    #+#              */
+/*   Updated: 2026/05/07 20:34:09 by srosu           ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ static float	count_disorder(t_stack *stack, size_t stack_size, t_list *node)
 	t_list	*next_node;
 
 	total_pairs = stack_size * (stack_size - 1) / 2;
+	mistakes = 0;
 	while (node)
 	{
 		next_node = node->next;
@@ -35,17 +36,10 @@ static float	count_disorder(t_stack *stack, size_t stack_size, t_list *node)
 
 float	compute_disorder(t_stack *stack)
 {
-	size_t	stack_size;
-	t_list	*node;
+	size_t	size;
 
-	stack_size = 0;
-	node = stack->head;
-	while (node)
-	{
-		stack_size++;
-		node = node->next;
-	}
-	if (stack_size < 2)
+	size = stack_size(stack);
+	if (size < 2)
 		return (0.0f);
-	return (count_disorder(stack, stack_size, node));
+	return (count_disorder(stack, size, stack->head));
 }
