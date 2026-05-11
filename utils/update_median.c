ffb0c63e486c7f363fe14835ec3aeb0d2f0a3eaf
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-05-11 13:14:34 +0200

    fix(median): fixed odd behaviour

--- utils/update_median.c
+++ utils/update_median.c
@@ -6,7 +6,7 @@
 /*   By: srosu <srosu@student.42belgium.be>        #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/05/11 08:47:07 by srosu            #+#    #+#              */
-/*   Updated: 2026/05/11 09:58:06 by srosu           ###   ########.fr        */
+/*   Updated: 2026/05/11 12:47:38 by srosu           ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,15 @@ void	update_median(t_stack *stack)
 	track = stack->head;
 	while (track)
 	{
-		if (track->current_position < size / 2)
+		if (track->current_position <= size / 2)
 			track->above_median = 1;
 		else
 			track->above_median = 0;
+		if (size % 2 == 1)
+		{
+			if (track->current_position == (size / 2) + 1)
+				track->above_median = 1;
+		}
 		track = track->next;
 	}
 }
