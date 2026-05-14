e4ec5c618b7b3c391704b3eac6200a0d7473bd0c
Sorin Rosu <sorinrosu.pro@gmail.com>
2026-05-14 15:11:45 +0200

    fix(check_flag): multiple flag behaviour covered

--- utils/flag_input.c
+++ utils/flag_input.c
@@ -12,6 +12,30 @@
 
 #include "../headers/push_swap.h"
 
+static int	reset(t_bool *flags, int strat)
+{
+	if (strat == 1)
+	{
+		flags->medium = 0;
+		flags->complex = 0;
+		return (flags->simple = 1);
+	}
+	else if (strat == 2)
+	{
+		flags->simple = 0;
+		flags->complex = 0;
+		return (flags->medium = 1);
+	}
+	else if (strat == 3)
+	{
+		flags->simple = 0;
+		flags->medium = 0;
+		return (flags->complex = 1);
+	}
+	return (0);
+}
+
+
 int	check_flag(char *str, t_bool *flags)
 {
 	if (ft_strcmp(str, "--bench") == 0)
@@ -19,10 +43,10 @@ int	check_flag(char *str, t_bool *flags)
 	if (ft_strcmp(str, "--adaptive") == 0)
 		return (flags->adaptive = 1);
 	else if (ft_strcmp(str, "--simple") == 0)
-		return (flags->simple = 1);
+		return (reset(flags, 1));
 	else if (ft_strcmp(str, "--medium") == 0)
-		return (flags->medium = 1);
+		return (reset(flags, 2));
 	else if (ft_strcmp(str, "--complex") == 0)
-		return (flags->complex = 1);
+		return (reset(flags, 3));
 	return (0);
 }
