82e6ba394598a27b3cccf15b5dcd1af57b4ef06a
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-05-18 18:34:39 +0200

    fix(is_valid): add checker if argv contain a minimum of a 1 number

--- utils/is_valid.c
+++ utils/is_valid.c
@@ -6,7 +6,7 @@
 /*   By: mbuchet <mbuchet@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 17:37:37 by srosu             #+#    #+#             */
-/*   Updated: 2026/05/15 03:53:38 by mbuchet          ###   ########.fr       */
+/*   Updated: 2026/05/18 18:31:25 by mbuchet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,35 @@
 int	is_valid(int c)
 {
 	return ((c >= '0' && c <= '9') || (c == ' ') || (c == '-'));
+}
+
+static int	is_argv_contain_number(int argc, char **argv, t_bool *flags)
+{
+	int	i;
+	int	j;
+	int	count_number;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (check_flag(argv[i], flags))
+		{
+			i++;
+			continue ;
+		}
+		count_number = 0;
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
+				count_number++;
+			j++;
+		}
+		if (count_number == 0)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 int	is_argv_is_valid(int argc, char **argv, t_bool *flags)
@@ -41,5 +70,5 @@ int	is_argv_is_valid(int argc, char **argv, t_bool *flags)
 		}
 		i++;
 	}
-	return (1);
+	return (is_argv_contain_number(argc, argv, flags));
 }
